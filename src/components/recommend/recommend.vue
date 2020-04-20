d24fc12256b4ab202b94bd5a5bf2c65f8a25b5ca
o-t-t <1310794052xu>
2020-04-20 22:01:24 +0800

    vue-lazyload懒加载插件应用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
           <ul>
             <li class='item' v-for='(item,index) in discList' :key='index'>
               <div class='icon'>
-                <img :src="item.imgurl" width='60' height='60' alt="">
+                <img v-lazy="item.imgurl" width='60' height='60' alt="">
               </div>
               <div class='text'>
                 <h2 class='name' v-html='item.creator.name'></h2>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('common/images/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
