d65d821d688aefa9aae8f68ff876030f2c0b1349
o-t-t <1310794052xu>
2020-04-20 21:51:29 +0800

    scroll组件的抽象和应用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,34 +1,39 @@
 <template>
   <div class='recommend'>
-    <div class='slider-wrapper'>
-      <slider>
-        <div v-for='(item,index) in recommends' :key='index'>
-          <a :href="item.linkUrl">
-            <img :src="item.picUrl" alt="" class='neddsclick'>
-          </a>
+    <scroll :data='discList' class='recommend-content' ref='scroll'>
+      <div>
+        <div class='slider-wrapper' v-if="recommends.length">
+          <slider>
+            <div v-for='(item,index) in recommends' :key='index'>
+              <a :href="item.linkUrl">
+                <img @load='loadImage' :src="item.picUrl" alt="" class='neddsclick'>
+              </a>
+            </div>
+          </slider>
         </div>
-      </slider>
-    </div>
-    <div class='recommend-list'>
-      <h1 class='list-title'>热门歌单推荐</h1>
-      <ul>
-        <li class='item' v-for='(item,index) in discList' :key='index'>
-          <div class='icon'>
-            <img :src="item.imgurl" width='60' height='60' alt="">
-          </div>
-          <div class='text'>
-            <h2 class='name' v-html='item.creator.name'></h2>
-            <p class='desc' v-html='item.dissname'></p>
-          </div>
-        </li>
-      </ul>
-    </div>
+        <div class='recommend-list'>
+          <h1 class='list-title'>热门歌单推荐</h1>
+          <ul>
+            <li class='item' v-for='(item,index) in discList' :key='index'>
+              <div class='icon'>
+                <img :src="item.imgurl" width='60' height='60' alt="">
+              </div>
+              <div class='text'>
+                <h2 class='name' v-html='item.creator.name'></h2>
+                <p class='desc' v-html='item.dissname'></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </scroll>
   </div>
 </template>
 <script>
 import {getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
 export default {
   data() {
     return {
@@ -37,7 +42,8 @@ export default {
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   created() {
     this._getRecommend()
@@ -61,6 +67,12 @@ export default {
           this.discList = res.data.list
         }
       })
+    },
+    loadImage() {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.scroll.refresh()
+      }
     }
   }
 }
@@ -73,37 +85,40 @@ export default {
     width: 100%
     top: 88px;
     bottom: 0
-    .slider-wrapper
-      position: relative
-      width: 100%;
+    .recommend-content
+      height: 100%
       overflow: hidden
-    .recommend-list
-      .list-title
-        height: 65px
-        line-height: 65px
-        text-align: center
-        font-size: $font-size-medium
-        color: $color-theme
-      .item
-        display: flex
-        box-sizing: border-box
-        align-items: center
-        padding: 0 20px 20px 20px
-        .icon
-          flex: 0 0 60px
-          width: 60px
-          padding-right: 20px
-        .text
-          display: flex
-          flex-direction: column
-          justify-content: center
-          flex: 1
-          line-height: 20px
-          overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%;
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
           font-size: $font-size-medium
-          .name
-            margin-bottom: 10px
-            color: $color-text
-          .desc
-            color: $color-text-d
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
 </style>
